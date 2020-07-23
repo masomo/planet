413359620f0dca07ac516d08182267f40471d405
Mehmet KARCI <mk@mkrc.net>
2020-07-23 16:58:18 +0300

    Update playernetwork.go

--- playernetwork.go
+++ playernetwork.go
@@ -14,7 +14,7 @@ import (
 const (
 	AckSize                   = 32
 	RttSmoothingFactor        = 0.0025
-	PacketLossSmoothingFactor = 0.75
+	PacketLossSmoothingFactor = 1.0
 	BandwidthSmoothingFactor  = 0.10
 )
 
